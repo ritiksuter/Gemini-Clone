fbe1a371810af1be8d125f234daba25b2bedbbf6
Ritik Suteri <ritiksuteri@gmail.com>
2024-10-19 13:03:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./Components/Main/Main";
+import SideBar from "./Components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <>
+      <SideBar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App;
